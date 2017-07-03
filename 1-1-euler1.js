acdfa59acca2f2626460fb2b3fce1bd2b337f65c
Brandon Neri <arcaniteamp@gmail.com>
2017-07-02 20:42:10 -0500

    Post 1 - Solución 1 - Project Euler #1

--- 1-1-euler1.js
+++ 1-1-euler1.js
@@ -1,7 +1,7 @@
 function sumaMultiplos3y5() {
   var suma = 0;
   for (var i = 0; i < 1000; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
+    if (i % 3 == 0 || i % 5 == 0) {
       suma += i;
     }
   }
